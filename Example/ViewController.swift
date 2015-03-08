4f683f310f28f534d81251d7da86f6d1fd8d4f81
Morten Bek Ditlevsen <bek@termestrup.dk>
2015-03-08 10:50:46 +0100

    Added template for an example project using SmappeeKit

---
+++ Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Morten Bek Ditlevsen on 08/03/15.
+//  Copyright (c) 2015 Mojo Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, SmappeControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
--- Podfile
+++ Podfile
@@ -1,7 +1,7 @@
 # Uncomment this line to define a global platform for your project
 # platform :ios, '6.0'
 
-link_with 'SmappeeKit', 'SmappeeKitTests'
+link_with 'SmappeeKit', 'SmappeeKitTests', 'Example'
 
 pod 'SwiftyJSON', :git => 'https://github.com/SwiftyJSON/SwiftyJSON.git', :branch => 'xcode6.3'
 
